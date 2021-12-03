c0de3cab82873fe923418e4f2655e6907c829a89
William Dang <William.Dang@stonybrook.edu>
2021-12-03 16:25:58 -0500

    forgot to print out the quests

--- Midterm Project/Midterm Project/main.cpp
+++ Midterm Project/Midterm Project/main.cpp
@@ -324,6 +324,12 @@ void playerOptions(player& p1, fileOperations& files, scoreboard& p1Scoreboard) 
 			break;
 		case '5':
 			cout << "\nYou selected 'Save and Quit'" << endl;
+			cout << "Quests Completed:" << endl;
+			while (!questCompleted.empty())
+			{
+				cout << questCompleted.front()<<endl;
+				questCompleted.pop();
+			}
 			files.save2File(p1);
 			files.save2File(p1Scoreboard);
 			files.closeFile();
@@ -342,19 +348,31 @@ void checkQuest(player& p1, scoreboard& p1Scoreboard)
 	{
 	case 1:
 		if (p1.getWeapon() == "Hand")
+		{
 			questCompleted.push("Don't Need a Weapon");
+			p1Scoreboard.addBonus(1);//temp value
+		}
 		break;
 	case 2:
 		if (p1.getBal() == 10)
+		{
 			questCompleted.push("Money in the Bank");
+			p1Scoreboard.addBonus(1);//temp value
+		}
 		break;
 	case 3:
 		if (p1.getDMG() > p1.getMaxHP())
+		{
 			questCompleted.push("Glass Cannon");
+			p1Scoreboard.addBonus(1);//temp value
+		}
 		break;
 	case 4:
 		if ((p1.getHP() / p1.getMaxHP()) > 0.2)
+		{
 			questCompleted.push("Just a Scratch");
+			p1Scoreboard.addBonus(1);//temp value
+		}
 		break;
 	default:
 		break;
